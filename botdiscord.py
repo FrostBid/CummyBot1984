ca71367389136811ef86b1ecde24c8bf2cc1aef4
FrostBid <83489811+FrostBid@users.noreply.github.com>
2021-10-11 19:40:10 +0800

    finalised discord version 1.0

--- botdiscord.py
+++ botdiscord.py
@@ -1,8 +1,9 @@
-import asyncpraw
 from config import *
+from badwords import badwords
+import praw
+import asyncpraw
 from time import sleep
 from better_profanity import profanity
-from badwords import badwords
 from datetime import datetime
 import discord
 import os
@@ -23,17 +24,18 @@ import os
 client = discord.Client()
 
 print("Logging in Reddit...")
-reddit = asyncpraw.Reddit(username=username,
-                          password=password,
-                          client_id=client_id,
-                          client_secret=client_secret,
-                          user_agent=user_agent)
+reddit = praw.Reddit(username=username,
+                     password=password,
+                     client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     check_for_async=False)
 print("Logged in Reddit!")
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in Discord as {0.user}'.format(client))
 
 
 def comment(op, body):  # Filter Spam
@@ -41,8 +43,8 @@ def comment(op, body):  # Filter Spam
         print(f"{op.name}'s karma is too low. Bypassing submission.")
         error = 'toolong'
         return False
-    elif len(body) > 9999:
-        body = body[:9990]
+    elif len(body) > 9900:
+        body = body[:9900]
     return True
 
 
@@ -57,23 +59,26 @@ def reply(title, body):
 
 @client.event
 async def on_message(message):
-    subreddit = await reddit.subreddit("copypasta")
+    subreddit = reddit.subreddit("copypasta")
     channel = client.get_channel(channelid)
-    async for submission in subreddit.stream.submissions(skip_existing=True):
+    for submission in subreddit.stream.submissions(skip_existing=True):
         title = submission.title
         body = submission.selftext
         op = submission.author
-        await op.load()
+        try:
+            if comment(op, body):
+                submission.reply(reply(title, body))
+                print(f'Replied to {op.name}')
+                time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+                await channel.send(f"{time} --- Replied to {op.name}.")
+                sleep(10)
 
-        if comment(op, body):
-            await submission.reply(reply(title, body))
-            print(f'Replied to {op.name}')
+            if not comment(op, body):
+                time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+                await channel.send(f"{time} --- Bypassed {op.name}, karma too low.")
+        except:
             time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
-            await channel.send(f"{time} --- Replied to {op.name}.")
-            sleep(10)
-        if not comment(op, body):
-            time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
-            await channel.send(f"{time} --- Bypassed {op.name}, karma too low.")
+            await channel.send(f"{time} --- Error has occurred!! @FrostBid#4135")
 
 
 if __name__ == "__main__":
